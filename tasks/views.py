06716640668187a1a18f8afbbbd4917b97b0ec95
tyv-m <look2mars@gmail.com>
2026-04-28 18:51:24 -0400

    create home view to display tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
+from .models import Task
+
+def home(request):
+    tasks = Task.objects.all()
+    return render(request, 'home.html', {'tasks': tasks})

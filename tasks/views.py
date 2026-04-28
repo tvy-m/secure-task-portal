4264517df49a90d4558c258812fb8509dfe281f5
tyv-m <look2mars@gmail.com>
2026-04-28 19:07:15 -0400

    able to add and store tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,21 @@
 # Create your views here.
 from django.shortcuts import render
 from .models import Task
+from .forms import TaskForm
 
 def home(request):
     tasks = Task.objects.all()
     return render(request, 'home.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect('home')
+    else:
+        form = TaskForm()
+
+    return render(request, 'add_task.html', {'form': form})
